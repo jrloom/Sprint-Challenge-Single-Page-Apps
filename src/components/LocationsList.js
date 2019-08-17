fae24eafc1626fe7e59482282ee91a9c6ccc4c00
Jason Loomis <jason@jrloomis.com>
2019-08-17 13:06:46 -0400

    list setup

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,24 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard";
 
-export default function LocationsList() {}
+const LocationList = props => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("")
+      .then(resolve => setList(resolve.data.results))
+      .catch(error => console.log("omg locations > ", error));
+  }, [props.match.path]);
+
+  return (
+    <section className="location-list grid-view">
+      {list.map(location => {
+        return <LocationCard key={location.id} {...location} />;
+      })}
+    </section>
+  );
+};
+
+export default LocationsList;
