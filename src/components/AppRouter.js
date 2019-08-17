27db35ab5f64ad3399e6edc1ea2586ce74d7cf33
Jason Loomis <jason@jrloomis.com>
2019-08-17 13:11:56 -0400

    routes routed

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
+import CharacterList from "./CharacterList";
+import LocationList from "./LocationsList";
+import EpisodeList from "./EpisodeList";
 
 const AppRouter = () => {
   return (
     <div>
       <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" render={props => <CharacterList {...props} />} />
+      <Route path="/locations" render={props => <LocationList {...props} />} />
+      <Route path="/episodes" render={props => <EpisodeList {...props} />} />
     </div>
   );
 };
