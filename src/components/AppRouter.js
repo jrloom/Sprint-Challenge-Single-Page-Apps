37ae1b01fc56b119db0502491ab36330f1d24460
Jason Loomis <jason@jrloomis.com>
2019-08-17 12:21:24 -0400

    setup app router

---
+++ src/components/AppRouter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <div />
+    </div>
+  );
+};
+
+export default AppRouter;
