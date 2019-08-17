d357f4469e95ce2d401db954bc6a69fe0311cd12
Jason Loomis <jason@jrloomis.com>
2019-08-17 12:54:56 -0400

    list setup

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+const CharacterList = props => {
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get("")
+      .then(resolve => setList(resolve.data.results))
+      .catch(error => console.log("omg character > ", error));
+  }, [props.match.path]);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {list.map(character => {
+        return <CharacterCard key={character.id} character={character} />;
+      })}
     </section>
   );
-}
+};
+
+export default CharacterList;
