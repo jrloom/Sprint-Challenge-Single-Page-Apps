bbb43d6a0889bfe8e306762250157acb242d4c1e
Jason Loomis <jason@jrloomis.com>
2019-08-17 12:38:02 -0400

    placeholder card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
-}
+const LocationCard = ({ name, type, dimension, residents }) => {
+  return (
+    <Card>
+      <Card.Header>Name</Card.Header>
+      <Card.Content>Type</Card.Content>
+      <Card.Content>Dimension</Card.Content>
+      <Card.Content>Residents</Card.Content>
+    </Card>
+  );
+};
+
+export default LocationCard;
